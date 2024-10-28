7a164492ae87992f55698723bc68c4210bf195b3
miseryMMM <164722628+miseryMMM@users.noreply.github.com>
2024-10-28 23:44:14 +0700

    Add files via upload

---
+++ Grass-Claimer-master/data/config.py
@@ -0,0 +1,7 @@
+THREADS = 1
+
+# rpc https://www.quicknode.com/login
+RPC_URL = 'https://greatest-summer-diagram.solana-mainnet.quiknode.pro/ea60cb220d957049e3e29fc07f34853f4d35313b'
+DESTINATION_ADDRESS = ''
+
+TIP_AMOUNT = 0.195  # 0.05 -> 1 in %
